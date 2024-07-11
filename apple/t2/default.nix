3d1e772eb49bc2c04e7be8ed3c089bdc44e02af6
Cassie Cheung <me@soopy.moe>
2024-07-11 20:48:45 +0800

    apple/t2: expunge tiny-dfr

--- apple/t2/default.nix
+++ apple/t2/default.nix
@@ -24,8 +24,6 @@ let
 
   pipewirePackage = overrideAudioFiles pkgs.pipewire "spa/plugins/";
 
-  tiny-dfrPackage = pkgs.callPackage ./pkgs/tiny-dfr.nix { };
-
   apple-set-os-loader-installer = pkgs.stdenv.mkDerivation {
     name = "apple-set-os-loader-installer-1.0";
     src = pkgs.fetchFromGitHub {
@@ -58,7 +56,7 @@ in
     boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ./pkgs/linux-t2.nix { });
     boot.initrd.kernelModules = [ "apple-bce" ];
 
-    services.udev.packages = [ audioFilesUdevRules tiny-dfrPackage ];
+    services.udev.packages = [ audioFilesUdevRules ];
 
     # For audio
     boot.kernelParams = [ "pcie_ports=compat" "intel_iommu=on" "iommu=pt" ];
@@ -68,22 +66,6 @@ in
     services.pipewire.package = pipewirePackage;
     services.pipewire.wireplumber.package = pkgs.wireplumber.override {
       pipewire = pipewirePackage;
-    };
-
-    # For tiny-dfr
-    systemd.services.tiny-dfr = {
-      enable = true;
-      description = "Tiny Apple silicon touch bar daemon";
-      after = [ "systemd-user-sessions.service" "getty@tty1.service" "plymouth-quit.service" "systemd-logind.service" ];
-      bindsTo = [ "dev-tiny_dfr_display.device" "dev-tiny_dfr_backlight.device" ];
-      startLimitIntervalSec = 30;
-      startLimitBurst = 2;
-      script = "${tiny-dfrPackage}/bin/tiny-dfr";
-      restartTriggers = [ tiny-dfrPackage ];
-    };
-
-    environment.etc."tiny-dfr/config.toml" = {
-      source = "${tiny-dfrPackage}/share/tiny-dfr/config.toml";
     };
 
     # Make sure post-resume.service exists
